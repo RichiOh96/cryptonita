ec59860847e3724c8fd0182cf75eb76b86a4a846
Ricardo Ortiz <misterlroa@gmail.com>
2021-04-11 15:39:20 -0600

    First commit

---
+++ src/main/java/com/fortilux/cryptonita/CryptonitaApplication.java
@@ -0,0 +1,13 @@
+package com.fortilux.cryptonita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptonitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptonitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fortilux/cryptonita/CryptonitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortilux.cryptonita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptonitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
